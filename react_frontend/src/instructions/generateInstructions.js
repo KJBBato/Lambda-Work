c8e346752b4c824b8bb0a8431091e91f60676ffb
TazoBlackTea <bato.kevin@gmail.com>
2019-05-22 15:18:20 -0400

    cleaned up code

--- react_frontend/src/instructions/generateInstructions.js
+++ react_frontend/src/instructions/generateInstructions.js
@@ -4,7 +4,6 @@ import Styles from '../styles'
 class GenerateInstructions extends React.Component {
   render() {
     const instruction = 'Welcome';
-    console.log('hi mom')
     return(
       <div style={Styles.instructionSize}>
         these are the generate app instrutions
